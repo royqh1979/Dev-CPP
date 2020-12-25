04f344490ca559cbe4d56a411f831a4535562dce
royqh1979@gmail.com <Roy>
2020-12-25 08:33:36 +0800

    - remove no use tools

--- Source/Tools/InitDevCppConfig/resource.h
+++
@@ -1,7 +0,0 @@
-#ifndef IDC_STATIC
-#define IDC_STATIC (-1)
-#endif
-
-#define IDD_MAIN_DIALOG                         101
-#define IDR_DEVCPP                              102
-#define IDR_CODE_INSERTION                      103
--- Source/Tools/PackMaker/filefrm.pas
+++ Source/Tools/PackMaker/filefrm.pas
@@ -44,13 +44,15 @@ type
     procedure SetMode(d : boolean);
     { Public declarations }
   end;
-
+  
+  function NewSelectDirectory(const Caption: string; const Root: WideString;
+    var Directory: string): Boolean;
 var
   FileForm: TFileForm;
 
 implementation
 
-uses main;
+uses main,shlobj,ActiveX;
 
 {$R *.dfm}
 
@@ -69,13 +71,81 @@ procedure TFileForm.LoadBtnClick(Sender: TObject);
 var s : string;
 begin
   if Dir then begin
-    if NewSelectDirectory('Select Directory', ExtractFilePath(MainForm.FileName), s) then
+    if NewSelectDirectory('Choose Directory',ExtractFilePath(MainForm.FileName),s) then
       edSource.Text := ExtractRelativePath(MainForm.FileName, s);
   end
   else begin
     OpenDialog.InitialDir := ExtractFilePath(MainForm.FileName);
     if OpenDialog.Execute then
       edSource.Text := ExtractRelativePath(MainForm.FileName, OpenDialog.FileName);
+  end;
+end;
+
+function SelectDirCB(Wnd: HWND; uMsg: UINT; lParam, lpData: LPARAM): Integer stdcall;
+begin
+  if (uMsg = BFFM_INITIALIZED) and (lpData <> 0) then
+    SendMessage(Wnd, BFFM_SETSELECTION, Integer(True), lpdata);
+  result := 0;
+end;
+
+function NewSelectDirectory(const Caption: string; const Root: WideString;
+  var Directory: string): Boolean;
+var
+  WindowList: Pointer;
+  BrowseInfo: TBrowseInfo;
+  Buffer: PChar;
+  OldErrorMode: Cardinal;
+  RootItemIDList, ItemIDList: PItemIDList;
+  ShellMalloc: IMalloc;
+  IDesktopFolder: IShellFolder;
+  Eaten, Flags: LongWord;
+begin
+  Result := False;
+  if not DirectoryExists(Directory) then
+    Directory := '';
+  FillChar(BrowseInfo, SizeOf(BrowseInfo), 0);
+  if (ShGetMalloc(ShellMalloc) = S_OK) and (ShellMalloc <> nil) then
+  begin
+    Buffer := ShellMalloc.Alloc(MAX_PATH);
+    try
+      RootItemIDList := nil;
+      if Root <> '' then
+      begin
+        SHGetDesktopFolder(IDesktopFolder);
+        IDesktopFolder.ParseDisplayName(Application.Handle, nil,
+          POleStr(Root), Eaten, RootItemIDList, Flags);
+      end;
+      with BrowseInfo do
+      begin
+        hwndOwner := Application.Handle;
+        pidlRoot := RootItemIDList;
+        pszDisplayName := Buffer;
+        lpszTitle := PChar(Caption);
+        ulFlags := BIF_RETURNONLYFSDIRS or BIF_USENEWUI;
+        if Directory <> '' then
+        begin
+          lpfn := SelectDirCB;
+          lParam := Integer(PChar(Directory));
+        end;
+      end;
+      WindowList := DisableTaskWindows(0);
+      OldErrorMode := SetErrorMode(SEM_FAILCRITICALERRORS);
+      try
+        ItemIDList := ShBrowseForFolder(BrowseInfo);
+      finally
+        SetErrorMode(OldErrorMode);
+        EnableTaskWindows(WindowList);
+      end;
+      Result :=  ItemIDList <> nil;
+      if Result then
+      begin
+        ShGetPathFromIDList(ItemIDList, Buffer);
+        ShellMalloc.Free(ItemIDList);
+        Directory := Buffer;
+      end;
+    finally
+      ShellMalloc.Free(Buffer);
+    end;
   end;
 end;
 
